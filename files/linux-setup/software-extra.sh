1878a301615f4fa91c85903d6350c1d4657953b2
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-12-25 10:09:11 +0000

    add better discord installation to script

--- files/linux-setup/software-extra.sh
+++ files/linux-setup/software-extra.sh
@@ -1,7 +1,12 @@
 echo "============ INSTALLING EXTRA SOFTWARE PACKADGES... ============"
-sudo pacman -S discord gnome-boxes  
+sudo pacman -S discord gnome-boxes
+
 
 paru -S jekyll
 
 flatpak install flathub com.vscodium.codium io.github.shiftey.Desktop com.microsoft.Teams
 echo "======= ...FINISHED INSTALLING EXTRA SOFTWARE PACKADGES! ======="
+
+#echo "================= INSTALLING BETTER DISCORD... ================="
+#echo ""
+#echo "============...FINISHED INSTALLING BETTER DISCORD... ==========="
