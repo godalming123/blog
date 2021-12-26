f563b21ba640fdba61fa76112d448e0b61b9ccc0
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-12-26 18:42:17 +0000

    fix bashrc

--- files/linux-setup/desktop-setup.sh
+++ files/linux-setup/desktop-setup.sh
@@ -43,7 +43,7 @@ sudo curl -o   /usr/share/backgrounds/bspwm/windows.jpg           https://godalm
 sudo curl -o   /usr/share/backgrounds/bspwm/detailed-mountain.jpg https://godalming123.github.io/blog/files/linux-setup/configs/wallapapers/detailed-mountain.jpg
 sudo curl -o   /usr/share/backgrounds/bspwm/simple-mountain.jpg   https://godalming123.github.io/blog/files/linux-setup/configs/wallapapers/simple-mountain.jpg
 
-curl -o        ~/.bashrc                           https://godalming123.github.io/blog/files/linux-setup/configs/.bashrc
+curl -o        ~/.bashrc                           https://godalming123.github.io/blog/files/linux-setup/configs/bashrc
 curl -o        ~/.welcome_screen                   https://godalming123.github.io/blog/files/linux-setup/configs/bash-welcome
 
 mkdir          ~/.config/alacritty/
