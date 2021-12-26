d42e7e1f06f1a9459c9c0a582f8f3d766b345ad0
godalming123 <r2hk9ahnf@relay.firefox.com>
2021-12-26 12:42:45 +0000

    add lightdm configuration and change screenlayout so the laptop screen is diabled also configure moniters when lightdm boots instead of when bspwm boots

--- files/linux-setup/desktop-setup.sh
+++ files/linux-setup/desktop-setup.sh
@@ -33,34 +33,37 @@ echo "STARTING DESKTOP SETUP..."
 mkdir ~/.config/
 
 # add config files
-curl -o        ~/.bashrc                         https://godalming123.github.io/blog/files/linux-setup/configs/.bashrc
-curl -o        ~/.welcome_screen                 https://godalming123.github.io/blog/files/linux-setup/configs/bash-welcome
+mkdir          /etc/lightdm
+curl -o        /etc/lightdm/lightdm.conf           https://godalming123.github.io/blog/files/linux-setup/configs/lightdm.conf
+
+curl -o        ~/.bashrc                           https://godalming123.github.io/blog/files/linux-setup/configs/.bashrc
+curl -o        ~/.welcome_screen                   https://godalming123.github.io/blog/files/linux-setup/configs/bash-welcome
 
 mkdir          ~/.config/alacritty/
-curl -o        ~/.config/alacritty/alacritty.yml https://godalming123.github.io/blog/files/linux-setup/configs/alacritty.yml
+curl -o        ~/.config/alacritty/alacritty.yml   https://godalming123.github.io/blog/files/linux-setup/configs/alacritty.yml
 
 mkdir          ~/.config/bspwm/
-curl -o        ~/.config/bspwm/bspwmrc           https://godalming123.github.io/blog/files/linux-setup/configs/bspwmrc
+curl -o        ~/.config/bspwm/bspwmrc             https://godalming123.github.io/blog/files/linux-setup/configs/bspwmrc
 chmod a+x      ~/.config/bspwm/bspwmrc
 
-mkdir          ~/.screenlayout/
-curl -o        ~/.screenlayout/workstation.sh    https://godalming123.github.io/blog/files/linux-setup/configs/screenlayout.sh
-chmod a+x      ~/.screenlayout/workstation.sh
+sudo mkdir     /usr/share/screenlayouts/
+sudo curl -o   /usr/share/screenlayouts/primary.sh https://godalming123.github.io/blog/files/linux-setup/configs/screenlayout.sh
+sudo chmod a+x /usr/share/screenlayouts/primary.sh
 
 mkdir          ~/.config/sxhkd/
-curl -o        ~/.config/sxhkd/sxhkdrc           https://godalming123.github.io/blog/files/linux-setup/configs/sxhkdrc
+curl -o        ~/.config/sxhkd/sxhkdrc             https://godalming123.github.io/blog/files/linux-setup/configs/sxhkdrc
 
 mkdir          ~/.config/picom/
-curl -o        ~/.config/picom/picom.conf        https://godalming123.github.io/blog/files/linux-setup/configs/picom.conf
+curl -o        ~/.config/picom/picom.conf          https://godalming123.github.io/blog/files/linux-setup/configs/picom.conf
 
 mkdir          ~/.config/polybar/
-curl -o        ~/.config/polybar/launch.sh       https://godalming123.github.io/blog/files/linux-setup/configs/polybar-launch.sh
+curl -o        ~/.config/polybar/launch.sh         https://godalming123.github.io/blog/files/linux-setup/configs/polybar-launch.sh
 chmod a+x      ~/.config/polybar/launch.sh
 
-curl -o        ~/.config/polybar/config          https://godalming123.github.io/blog/files/linux-setup/configs/polybar-config
+curl -o        ~/.config/polybar/config            https://godalming123.github.io/blog/files/linux-setup/configs/polybar-config
 
 mkdir          ~/.config/rofi/
-curl -o        ~/.config/rofi/config.rasi        https://godalming123.github.io/blog/files/linux-setup/configs/rofi-config.rasi
+curl -o        ~/.config/rofi/config.rasi          https://godalming123.github.io/blog/files/linux-setup/configs/rofi-config.rasi
 
 sudo mkdir     /usr/share/fonts/TTF/
 sudo curl -o   /usr/share/fonts/TTF/Material-Design-Iconic-Font.ttf https://godalming123.github.io/blog/files/linux-setup/configs/Material-Design-Iconic-Font.ttf
