db557c5eade3ceffe7d29f10366cec8449cf93ea
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-01-08 08:45:36 +0000

    add wofi config and use btop packadge from pacman instead of aur

--- files/linux-setup/software-basic.sh
+++ files/linux-setup/software-basic.sh
@@ -1,5 +1,3 @@
 echo "============ INSTALLING BASIC SOFTWARE PACKADGES... ============"
-sudo pacman -S micro alacritty git nautilus firefox
-
-paru -S btop
+sudo pacman -S micro alacritty git nautilus firefox btop
 echo "======= ...FINISHED INSTALLING BASIC SOFTWARE PACKADGES! ======="
