27384c6509ea9e140585fbed62641756a0e18e18
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-03-24 07:31:17 +0000

    remove linux setup files and fix service worker + manifest

--- files/linux-setup/software-basic.sh
+++
@@ -1,3 +0,0 @@
-echo "============ INSTALLING BASIC SOFTWARE PACKADGES... ============"
-sudo pacman -S micro alacritty git nautilus firefox btop
-echo "======= ...FINISHED INSTALLING BASIC SOFTWARE PACKADGES! ======="
